47662ae65b9355c33eec74a36b17ce99259757ea
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2024-08-24 00:36:29 +0100

    Update font for MenuButton

--- source/objects/MenuButton.hx
+++ source/objects/MenuButton.hx
@@ -2,11 +2,12 @@ package objects;
 
 import flixel.FlxSprite;
 import flixel.addons.ui.FlxButtonPlus;
+import flixel.system.FlxAssets;
 
 class MenuButton extends FlxButtonPlus
 {
 	public function new(X:Float = 0, Y:Float = 0, Callback:Dynamic, ?Label:String, Width:Int = 100, Height:Int = 20)
-	{
+	{	
 		var buttonNormalSprite = new FlxSprite(0, 0, AssetPaths.buttonDefault__png);
 		var buttonHighlightSprite = new FlxSprite(0, 0, AssetPaths.buttonHover__png);
 		super(X, Y, Callback, Label, Width, Height);
@@ -16,5 +17,7 @@ class MenuButton extends FlxButtonPlus
 		this.textHighlight.size = 12;
 		this.textNormal.y = 10;
 		this.textHighlight.y = 10;
+		this.textNormal.font = AssetPaths.ptSansRegular__ttf;
+		this.textHighlight.font = AssetPaths.ptSansRegular__ttf;
 	}
 }
